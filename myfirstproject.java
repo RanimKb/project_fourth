5fec2797301b0f12798be2c4955e7a68a9bcd0c5
RanimKb <49023006+RanimKb@users.noreply.github.com>
2019-03-29 11:44:00 +0300

    myfirstproject.java

---
+++ myfirstproject.java
@@ -0,0 +1,10 @@
+public class myproject{
+ String p="my first project";
+	public myproject (){
+		super();
+		x="we will modify the project here ";
+	}
+	public void printme (){
+		System.out.print("welcom !!!!!");
+	}
+}

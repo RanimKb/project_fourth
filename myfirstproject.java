cd41b5a4fe259740c8a67f02b98351939027775e
samahFA <samfakhoury84@gmail.com>
2019-03-29 18:35:55 +0300

    add c variable

--- myfirstproject.java
+++ myfirstproject.java
@@ -4,7 +4,9 @@ public class myproject{
 		super();
 		x="the first edit ";
 		y="the third edit ";
-                                z="the 4 edit";
+        z="the 4 edit";
+		c="the fivth edit ";
+								
 	}
 	public void printme (){
 		System.out.print("welcom !!!!!");

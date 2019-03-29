acad23aa61baa9bc88ce46b82780c88a4cddeecb
sajahes.997@gmail.com <12345roro6789>
2019-03-29 17:07:02 +0200

    modified the value y

--- myfirstproject.java
+++ myfirstproject.java
@@ -3,7 +3,7 @@ public class myproject{
 	public myproject (){
 		super();
 		x="the first edit ";
-		y="the second edit";
+		y="the third edit ";
 	}
 	public void printme (){
 		System.out.print("welcom !!!!!");

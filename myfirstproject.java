376c0b8efe436387e858086bd84a7ed409ecc243
samfakhoury84@gmail.com <12345roro6789>
2019-03-29 18:50:16 +0300

    add ss variable

--- myfirstproject.java
+++ myfirstproject.java
@@ -7,6 +7,7 @@ public class myproject{
         z="the 4 edit";
 		c="the fivth edit ";
 		s="the last edit"
+		ss="the last last edit"
 		}
 	public void printme (){
 		System.out.print("welcom !!!!!");

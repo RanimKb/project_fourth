be76022f782855a06f24d7d9ddc0d77c1f8f5f08
samahFA <samfakhoury84@gmail.com>
2019-03-29 18:42:07 +0300

    add s variable

--- myfirstproject.java
+++ myfirstproject.java
@@ -6,8 +6,8 @@ public class myproject{
 		y="the third edit ";
         z="the 4 edit";
 		c="the fivth edit ";
-								
-	}
+		s="the last edit"
+		}
 	public void printme (){
 		System.out.print("welcom !!!!!");
 	}
